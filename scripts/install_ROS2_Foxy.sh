e998c695e1f375d7dde23c1132c92a2d9381fc45
KeeTaek Yang <samscientist@naver.com>
2023-12-05 01:00:06 +0900

    Modify ROS2 installation script

--- scripts/install_ROS2_Foxy.sh
+++ scripts/install_ROS2_Foxy.sh
@@ -17,7 +17,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 sudo apt update
 sudo apt upgrade
 
-sudo apt install ros-foxy-desktop -y python3-argcomplete -y
+sudo apt install ros-foxy-desktop python3-argcomplete -y
 
 echo source /opt/ros/foxy/setup.bash >> ~/.bashrc
 source ~/.bashrc
+
+sudo apt install python3-colcon-common-extensions python3-rosdep
