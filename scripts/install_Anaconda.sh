969e4c3d0764a144c10e25f9aded4e77b44c0311
KeeTaek Yang <samscientist@naver.com>
2023-12-07 00:36:50 +0900

    Fix typo in script - `install_Anaconda.sh`

--- scripts/install_Anaconda.sh
+++ scripts/install_Anaconda.sh
@@ -1,6 +1,6 @@
 # Downloading installation script
 # TODO: Bring always the latest version. Currently, the URL points to specific version(which was the latest when this script was written).
-wget -P ~/Downloads/https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-aarch64.sh
+wget -P ~/Downloads/ https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-aarch64.sh
 bash ~/Downloads/Anaconda3-2023.09-0-Linux-aarch64.sh
 
 # Add PATH environment variable
